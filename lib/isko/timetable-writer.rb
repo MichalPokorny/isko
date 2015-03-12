7cf10565035eef3e3553a5943a847d727e532ada
Michal Pokorný <pok@rny.cz>
2015-03-12 11:56:16 +0100

    Add parallel timetable writer

--- lib/isko/timetable-writer.rb
+++ lib/isko/timetable-writer.rb
@@ -7,5 +7,29 @@ module Isko
 			engine = Haml::Engine.new(File.read('_timetable_template.html.haml'))
 			File.write(file, engine.render(Object.new, agent: agent, slots: slots))
 		end
+
+		def self.report_results_parallel(agent, slots, file)
+			day_rows = [[], [], [], [], []]
+
+			# Very suboptimal and hungry
+			for slot in slots
+				day = slot.start_day
+				rows_of_day = day_rows[day]
+
+				inserted = false
+				for row in rows_of_day
+					if row.none? { |other| other.collision?(slot) }
+						row << slot
+						inserted = true
+						break
+					end
+				end
+
+				rows_of_day << [slot] unless inserted
+			end
+
+			engine = Haml::Engine.new(File.read('_timetable_parallel.html.haml'))
+			File.write(file, engine.render(Object.new, agent: agent, day_rows: day_rows))
+		end
 	end
 end
